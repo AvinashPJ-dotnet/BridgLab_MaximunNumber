2906577119094c606628bf9cd3d37f1761458e7f
AvinashPJ-dotnet <avinash.pujari42@gmail.com>
2022-04-07 15:12:51 +0530

    Added: find maximum float number function

--- src/com/bl/test_maximum/find_maximum/Main.java
+++ src/com/bl/test_maximum/find_maximum/Main.java
@@ -2,12 +2,23 @@ package com.bl.test_maximum.find_maximum;
 
 public class Main {
     public static void main(String[] args) {
-        Integer max=testMaximumNumber(9, 3, 6);
-        System.out.println("maximum number is: "+max);
+        Integer maxInt=testMaximumNumber(9, 3, 6);
+        Float maxFloat=testMaximumNumber(9.4f, 3.5f, 9.9f);
+        System.out.println("maximum integer is: "+maxInt);
+        System.out.println("maximum float is: "+maxFloat);
     }
 
     private static Integer testMaximumNumber(Integer first, Integer second, Integer third) {
         Integer max = first;
+        if(second.compareTo(max)>0)
+            max=second;
+        if(third.compareTo(max)>0)
+            max=third;
+        return max;
+    }
+
+    private static Float testMaximumNumber(Float first, Float second, Float third) {
+        Float max = first;
         if(second.compareTo(max)>0)
             max=second;
         if(third.compareTo(max)>0)
