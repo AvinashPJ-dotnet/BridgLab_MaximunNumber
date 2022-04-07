435a7187db9377cd572dea1ed9e170381f467ad5
AvinashPJ-dotnet <avinash.pujari42@gmail.com>
2022-04-07 15:05:42 +0530

    Added: find maximum integer function

---
+++ src/com/bl/test_maximum/find_maximum/Main.java
@@ -0,0 +1,17 @@
+package com.bl.test_maximum.find_maximum;
+
+public class Main {
+    public static void main(String[] args) {
+        Integer max=testMaximumNumber(9, 3, 6);
+        System.out.println("maximum number is: "+max);
+    }
+
+    private static Integer testMaximumNumber(Integer first, Integer second, Integer third) {
+        Integer max = first;
+        if(second.compareTo(max)>0)
+            max=second;
+        if(third.compareTo(max)>0)
+            max=third;
+        return max;
+    }
+}
