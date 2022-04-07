e2a4ce730db08ea92687a078ca9e6f8fc6b29345
AvinashPJ-dotnet <avinash.pujari42@gmail.com>
2022-04-07 15:28:14 +0530

    Added: find max String function

--- src/com/bl/test_maximum/find_maximum/Main.java
+++ src/com/bl/test_maximum/find_maximum/Main.java
@@ -2,13 +2,15 @@ package com.bl.test_maximum.find_maximum;
 
 public class Main {
     public static void main(String[] args) {
-        Integer maxInt=testMaximumNumber(9, 3, 6);
-        Float maxFloat=testMaximumNumber(9.4f, 3.5f, 9.9f);
+        Integer maxInt= testMaximum(9, 3, 6);
+        Float maxFloat= testMaximum(9.4f, 3.5f, 9.9f);
+        String maxString= testMaximum("Apple"  , "Peach", "Banana");
         System.out.println("maximum integer is: "+maxInt);
         System.out.println("maximum float is: "+maxFloat);
+        System.out.println("maximum String is: "+maxString);
     }
 
-    private static Integer testMaximumNumber(Integer first, Integer second, Integer third) {
+    private static Integer testMaximum(Integer first, Integer second, Integer third) {
         Integer max = first;
         if(second.compareTo(max)>0)
             max=second;
@@ -17,8 +19,17 @@ public class Main {
         return max;
     }
 
-    private static Float testMaximumNumber(Float first, Float second, Float third) {
+    private static Float testMaximum(Float first, Float second, Float third) {
         Float max = first;
+        if(second.compareTo(max)>0)
+            max=second;
+        if(third.compareTo(max)>0)
+            max=third;
+        return max;
+    }
+
+    private static String testMaximum(String first, String second, String third) {
+        String max = first;
         if(second.compareTo(max)>0)
             max=second;
         if(third.compareTo(max)>0)
